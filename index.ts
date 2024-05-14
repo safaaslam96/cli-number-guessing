1195f1fce2aa22bf310e91d90605c943cc7963f3
Safa Aslam <160883400+safaaslam96@users.noreply.github.com>
2024-05-14 06:44:13 +0500

    pushed code

---
+++ index.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answers = await inquirer.prompt([
+  {
+    name: "guessNumber",
+    type: "number",
+    message: "Please guess a number between 1-10: ",
+  },
+]);
+
+if (answers.guessNumber === randomNumber) {
+  console.log("Very good! You  did it");
+} else {
+  console.log("You guess wrong number");
+}
